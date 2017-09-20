ebcd32495457d898d9d0e5a853080c896511e305
Lars Haugseth <git@larshaugseth.com>
2017-09-20 20:40:24 +0200

    Fixed byte_at and lookup_color, added chars_at.

--- src/breadbin/image/multicolor.cr
+++ src/breadbin/image/multicolor.cr
@@ -21,10 +21,14 @@ class Breadbin::Image::Multicolor
   # to the "01", "10" and "11" bit pairs respectively
   def byte_at(x : Int32, y : Int32, clist : ColorList) : UInt8
     colors = [1, 0, 2].map {|i| clist[i] }
-    self[x..(x + 3), y].each.with_object([0_u8, 64_u8]) { |c, o|
-      o[0] += o[1] * lookup_color(c, colors)
-      o[1] >>= 2
-    }[0]
+    byte = 0_u8
+    mask = 64_u8
+    4.times do |i|
+      c = self[x + i, y]
+      byte += mask * lookup_color(c, colors)
+      mask >>= 2
+    end
+    return byte
   end
 
   # Get a 10 bytes representation of the 4x8 pixel cell at the given *col* and *row*,
@@ -54,6 +58,40 @@ class Breadbin::Image::Multicolor
       end.to_a.sum
     end.to_a
     [colors[0] * 16 + colors[1], colors[2], bytes].flatten
+  end
+
+  class TooManyUniqueChars < Exception; end
+  def chars_at(x : Int32, y : Int32, cols : Int32, rows : Int32, clist : ColorList, blank : Bool = true) : Array(UInt8)
+    charset = Array(UInt8).new(2048, 0_u8)
+    charmap = Array(UInt8).new(cols * rows, 0_u8)
+    chindex = Hash(Array(UInt8), UInt8).new
+    if blank
+      char = [0_u8] * 8
+      charset[0..7] = char
+      chindex[char] = 0_u8
+    end
+    rows.times do |row|
+      cols.times do |col|
+        xx = x + 4 * col
+        yy = y + 8 * row
+        if xx < 0 || xx >= width || yy < 0 ||  yy >= height
+          char = [0_u8] * 8
+        else
+          char = 8.times.map { |i| byte_at(xx, yy + i, clist) }.to_a
+        end
+        if chindex.has_key?(char)
+          # puts "Found: #{char}"
+          charmap[row * cols + col] = chindex[char]
+        else
+          # puts "Did not find: #{char}"
+          raise TooManyUniqueChars.new unless chindex.size < 256
+          charmap[row * cols + col] = chindex.size.to_u8
+          charset[(chindex.size * 8)..(chindex.size * 8 + 7)] = char
+          chindex[char] = chindex.size.to_u8
+        end
+      end
+    end
+    return charset + charmap
   end
 
   # Get a 63 bytes sprite representation of the 12x21 pixel region at the given
@@ -90,7 +128,7 @@ class Breadbin::Image::Multicolor
   # the array, or 0 if its not included
   private def lookup_color(c : UInt8, colors : Array(UInt8 | Array(UInt8))) : UInt8
     colors.each.with_index do |ci, i|
-      return (i + 1).to_uint8 if ci == c || (ci.is_a?(Array) && ci.includes?(c))
+      return (i + 1).to_u8 if ci == c || (ci.is_a?(Array) && ci.includes?(c))
     end
     return 0_u8
   end
