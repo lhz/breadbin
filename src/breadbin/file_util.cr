4fd0ed5a8afb1b1662470a6b9aa132d0f19fa338
Lars Haugseth <git@larshaugseth.com>
2017-09-20 16:09:49 +0200

    FileUtil.binwrite takes array of u8 as well as bytes.

--- src/breadbin/file_util.cr
+++ src/breadbin/file_util.cr
@@ -2,10 +2,16 @@ module Breadbin
   module FileUtil
     extend self
 
-    def binwrite(pathname : String, address : UInt16, content : Bytes)
+    def binwrite(pathname : String, address : UInt16, content : Bytes | Array(UInt8))
       File.open(pathname, "wb") do |file|
         file.write Bytes.new(2) { |i| [address % 256, address / 256][i].to_u8 }
-        file.write content
+        case content
+        when Bytes
+          file.write content
+        when Array(UInt8)
+          bytes = Bytes.new(content.size) { |i| content[i] }
+          file.write bytes
+        end
       end
     end
   end
