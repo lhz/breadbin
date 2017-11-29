df55faa9ea39bfe96d00b906a182d73f2aef6ea6
Lars Haugseth <git@larshaugseth.com>
2017-11-29 23:57:29 +0100

    Added test palette config.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,6 @@
 require "spec"
 require "../src/breadbin"
 
+ENV["BREADBIN_CONFIG_PATH"] = File.expand_path("spec/config")
+
 # Spec.add_formatter Spec::VerboseFormatter.new
--- src/breadbin/palette_config.cr
+++ src/breadbin/palette_config.cr
@@ -23,18 +23,22 @@ module Breadbin
     end
 
     private def self.load_config() : Config
-      palettes = JSON.parse(File.read(config_file))
-      palettes.each_with_object(Config.new) do |palette, config|
-        key = palette["name"].as_s
-        val = palette["spec"].as_s.split(',').map { |c| c.to_i(16) }
-        config[key] = val
-      end
+      set_config File.read(config_file)
     end
 
     private def self.config_file() : String
       path = ENV.fetch(PATH_ENV, DEFAULT_PATH)
       File.expand_path(File.join path, "palettes.json").tap do |path|
         raise ConfigNotFound.new(path) unless File.exists?(path)
+      end
+    end
+
+    private def self.set_config(config : String)
+      palettes = JSON.parse(config)
+      palettes.each_with_object(Config.new) do |palette, config|
+        key = palette["name"].as_s
+        val = palette["spec"].as_s.split(',').map { |c| c.to_i(16) }
+        config[key] = val
       end
     end
   end
