7f5a4ba4828f8faf04d75b1ffdab6fafc919fa4d
Lars Haugseth <git@larshaugseth.com>
2019-10-27 19:01:51 +0100

    Improved bgdetect tool some more.

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 
 default: test
 
+tools: bin/bgdetect
+
+bin/bgdetect: src/tools/bgdetect.cr
+	crystal build -o $@ $^
+
 test:
 	crystal spec -- --no-color
--- src/tools/bgdetect.cr
+++ src/tools/bgdetect.cr
@@ -4,20 +4,22 @@ if ARGV.size == 0 || ARGV.any? { |f| !f.ends_with?(".png") }
   abort "Usage: bgdetect <png-file>+"
 end
 
+# Color indices ordered by probability of being used as background
+COLORS = [0, 15, 12, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14]
+
 def detect_bgcolor(png_file)
   puts "Detecting background for #{png_file}"
   image = Breadbin::Image::Multicolor.from_png(png_file, {0, 0, 160, 200})
   image.colfix = "nearest"
-
-  fixes = 16.times.map { |c|
+  bg, fc = -1, 9999
+  COLORS.each do |c|
     image.to_bytes(c.to_u8)
-    [c, image.fixcount]
-  }
-
-  bg, fc = fixes.min_by { |(c, f)| f }
-  print "  Background #{bg}"
-  print " with #{fc} issues" if fc > 0
-  puts "."
+    if image.fixcount < fc
+      bg, fc = c, image.fixcount
+      break if image.fixcount == 0
+    end
+  end
+  puts ["  Background #{bg}", fc > 0 ? " with #{fc} issues" : "", "."].join
 rescue ex : Breadbin::Image::InvalidType
   STDERR.puts "  WARNING: #{ex.message}"
 end
