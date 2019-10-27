bc2a5920d99f50a3584b0c8c4ff6e2d711fe34d1
Lars Haugseth <git@larshaugseth.com>
2019-10-27 19:14:18 +0100

    Improved bgdetect tool some more.

--- src/tools/bgdetect.cr
+++ src/tools/bgdetect.cr
@@ -8,7 +8,6 @@ end
 COLORS = [0, 15, 12, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14]
 
 def detect_bgcolor(png_file)
-  puts "Detecting background for #{png_file}"
   image = Breadbin::Image::Multicolor.from_png(png_file, {0, 0, 160, 200})
   image.colfix = "nearest"
   bg, fc = -1, 9999
@@ -19,9 +18,13 @@ def detect_bgcolor(png_file)
       break if image.fixcount == 0
     end
   end
-  puts ["  Background #{bg}", fc > 0 ? " with #{fc} issues" : "", "."].join
+  if fc == 0
+    puts "[%2d] %s" % [bg, png_file]
+  else
+    STDERR.puts "[%2d] %s (WARNING: %d cells with issues)" % [bg, png_file, fc]
+  end
 rescue ex : Breadbin::Image::InvalidType
-  STDERR.puts "  WARNING: #{ex.message}"
+  STDERR.puts "ERROR: #{ex.message}"
 end
 
 ARGV.each do |file|
